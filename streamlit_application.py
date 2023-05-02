885fb72934f8e69fb174bbe30cac7bcdb1dbcb99
aprateem2023 <aprateem.roy.2020@gmail.com>
2023-05-03 01:09:03 +0530

    Update streamlit_application.py

--- streamlit_application.py
+++ streamlit_application.py
@@ -64,15 +64,15 @@ if st.button("Get Fruit List"):
     my_cnx.close()
     st.dataframe(my_data_rows)
     print(my_data_rows)
-#st.stop()
+st.stop()
 add_my_fruit = st.text_input('What fruit would you like to add?')
 if not add_my_fruit:
     st.error("Please add a fruit name.")
 else :
     if st.button("Add a Fruit to the List"):
-    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+        my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
     
-    back_from_function = insert_row_snowflake(add_my_fruit)
-    my_cnx.close()
-    st.text(back_from_function)
+        back_from_function = insert_row_snowflake(add_my_fruit)
+        my_cnx.close()
+        st.text(back_from_function)
     
