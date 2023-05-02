1702f8553b3a819a76f1747ac16b0ace85696219
aprateem2023 <aprateem.roy.2020@gmail.com>
2023-05-03 00:44:58 +0530

    Update streamlit_application.py

--- streamlit_application.py
+++ streamlit_application.py
@@ -13,7 +13,7 @@ def get_fruityvice_data(this_fruit_choice):
 def get_fruit_load_list(my_cnx):
     with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * from fruit_load_list")
-    return my_cur.fetchall()
+        return my_cur.fetchall()
 
 st.title("My Parents New Healthy Diner")
 st.header("Breakfast Favourites")
